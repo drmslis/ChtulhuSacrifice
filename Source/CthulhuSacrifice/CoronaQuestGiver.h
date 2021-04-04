3b081f3866aed4544f611a7a62de70eddf5b1488
Chepurko Oleg <oleg.c@scopicsoftware.com>
2021-04-04 23:07:13 +0700

    Add quest givers

---
+++ Source/CthulhuSacrifice/CoronaQuestGiver.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestGiver.h"
+#include "CoronaQuestGiver.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class CTHULHUSACRIFICE_API UCoronaQuestGiver : public UQuestGiver
+{
+	GENERATED_BODY()
+	
+public:
+    UCoronaQuestGiver();
+	
+};
---
+++ Source/CthulhuSacrifice/MapQuestGiver.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestGiver.h"
+#include "MapQuestGiver.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class CTHULHUSACRIFICE_API UMapQuestGiver : public UQuestGiver
+{
+	GENERATED_BODY()
+	
+public:
+    UMapQuestGiver();
+	
+};
